5e13518f8fff6b15988cbf218e29548222ea9370
CMcLeodC <109034799+CMcLeodC@users.noreply.github.com>
2024-02-18 19:39:28 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardValues = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const suitValues = ["♦", "♥", "♠", "♣"];
+
+  let randomNumCard = Math.floor(Math.random() * 13);
+  let randomNumSuit = Math.floor(Math.random() * 4);
+
+  document.getElementById("num").textContent = cardValues[randomNumCard];
+
+  const topSuitElement = document.getElementById("topSuit");
+  const bottomSuitElement = document.getElementById("bottomSuit");
+
+  topSuitElement.textContent = suitValues[randomNumSuit];
+  bottomSuitElement.textContent = suitValues[randomNumSuit];
+
+  if (suitValues[randomNumSuit] === "♦" || suitValues[randomNumSuit] === "♥") {
+    topSuitElement.classList.add("reds");
+    bottomSuitElement.classList.add("reds");
+  } else {
+    topSuitElement.classList.add("blacks");
+    bottomSuitElement.classList.add("blacks");
+  }
 };
